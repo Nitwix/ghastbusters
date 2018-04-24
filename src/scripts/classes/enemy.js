a0c78e3fea4ff8191f1eefee1df0d722b4a3fd49
Ninooch <ninachaton@gmail.com>
2018-04-24 14:24:25 +0200

    animation des ghast

--- src/scripts/classes/enemy.js
+++ src/scripts/classes/enemy.js
@@ -5,7 +5,8 @@ class Enemy extends Phaser.Sprite{
         this.player = player;
 
         this.scale.setTo(Phaser.Math.random(2,3));
-
+        this.animations.add("float",[0,1,2,3,4,5,6,7]);
+        this.animations.play("float",5,true);
         game.add.existing(this);
     }
 
--- src/scripts/states/preload.js
+++ src/scripts/states/preload.js
@@ -2,8 +2,8 @@ let preloadState = new Phaser.State();
 
 preloadState.preload = function(){
     game.load.spritesheet("player","assets/sprites/candideSprite.png",35,67);
-    game.load.spritesheet("ghast","assets/sprites/ghast.png",16,16);
-    game.load.spritesheet("redGhast","assets/sprites/redGhast.png",16,16);
+    game.load.spritesheet("ghast","assets/sprites/ghast.png",32,32);
+    game.load.spritesheet("redGhast","assets/sprites/redGhast.png",32,32);
 
     game.load.tilemap("map","assets/map/map.json",null,Phaser.Tilemap.TILED_JSON);
     game.load.spritesheet("Netherrack","assets/sprites/Netherrack.png",32,32);
