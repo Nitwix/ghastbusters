93590bf5b2c6d59623a23090d2883466f8407e49
Ninooch <ninachaton@gmail.com>
2018-05-08 14:43:20 +0200

    game over centré

--- src/scripts/states/gameOver.js
+++ src/scripts/states/gameOver.js
@@ -1,6 +1,6 @@
 let gameOverState = new Phaser.State();
 
 gameOverState.create = function(){
-    let txt = game.add.text(0,0,"GAME OVER", {fill:'white', fontSize: 50});
-    txt.alignIn(game.world, Phaser.CENTER);
+    let txt = game.add.text(250,300,"GAME OVER", {fill:'white', fontSize: 50});
+
 }
