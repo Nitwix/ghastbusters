07e83a4cad24c7729a6491b786c5d32537cf7759
Ninooch <ninachaton@gmail.com>
2018-05-08 14:18:40 +0200

    géré le temps

--- src/scripts/classes/timeMgr.js
+++ src/scripts/classes/timeMgr.js
@@ -1,9 +1,13 @@
 class TimeMgr {
-    constructor(waveTime){
-        this.waveTime = waveTime;
-        this.cmptTot = new Phaser.Timer();
+    constructor(){
+        this.waveTime = 0;
+        this.cmptTot = game.time.create();
+        this.timeAddition = 10;
+        this.tpsText;
     }
-    startCmptTot(){
+    start(){
+        this.tpsText = game.add.text(32,32,"temps");
+        this.tpsText.fixedToCamera = true;
         this.cmptTot.start();
     }
     exportTime(){
@@ -11,7 +15,14 @@ class TimeMgr {
     }
 
     addTime(){
-        this.waveTime += .9*this.cmptTot;
+        this.timeAddition*=0.9;
+        this.waveTime += this.timeAddition;
     }
 
+    update(){
+        console.log(this.waveTime);
+        if(this.waveTime-this.cmptTot.seconds<=0){
+            console.log("Game over");
+        }
+    }
 }
--- src/scripts/states/game.js
+++ src/scripts/states/game.js
@@ -14,17 +14,18 @@ var gameState = {
 
         // enemy1 = new Enemy(game, 50, 10, "ghast", 0, player);
         // enemies = game.add.group();
-        timeManager = new TimeManager();
-        timeManager.start();
 
         // var enemiesPos = [[165,112],[628,192],[195,370],[222,338],[48,37],[704,305],[496,211],[527,34],[488,452],[633,400],[748,289],[611,109],[640,62],[359,80],[355,406],[502,377]];
         timeMgr = new TimeMgr();
+        timeMgr.start();
         enemies = new Enemies(game, player);
         stateMgr = new StateMgr(enemies, timeMgr);
         stateMgr.initWave();
+
     },
     update: function(){
         updatePlayer();
+        timeMgr.update();
         enemies.updateEnemies();
 
         if(enemies.waveFinished){
